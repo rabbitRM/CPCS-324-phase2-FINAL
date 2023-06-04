bc58488443b84e3a641d1a0d71e24d4bc57dd9e1
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-06-04 20:28:55 +0300

    Update ShortestPathAlgorithm.java

--- CPCS324_Project_Phase2/src/GraphFramework/ShortestPathAlgorithm.java
+++ CPCS324_Project_Phase2/src/GraphFramework/ShortestPathAlgorithm.java
@@ -1,9 +1,27 @@
 package GraphFramework;
 
+
 public class ShortestPathAlgorithm {
     
    
     // Defining the data feilds
     protected static Graph graph  ;
+
+    
+    //-----------------------------------------------------------------------------
+    // Defining the constructor
+    public ShortestPathAlgorithm() {
+    }
    
+    
+    //-----------------------------------------------------------------------------
+    // Defining the setters and getterss
+    public static Graph getGraph() {
+        return graph;
+    }
+
+    public static void setGraph(Graph graph) {
+        ShortestPathAlgorithm.graph = graph;
+    }
+
 }
